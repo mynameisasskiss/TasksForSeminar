0ae4b22568ed564f8a36748031b6a8d16547e2cb
Anton Makarov <avamkakoedelovich@ya.ru>
2024-12-09 11:31:15 +0300

    Update 4_14.cpp

--- 4_14.cpp
+++ 4_14.cpp
@@ -2,14 +2,17 @@
 #include <cmath>
 
 void getRoots(double a, double b, double c) {
-    double D = b*b - 4*a*c;
-    double root1 = ((b*-1)+sqrt(D))/(2*a);
-    double root2 = ((b*-1)-sqrt(D))/(2*a);
-    if (isnan(root1)||isnan(root2)) {
-        std::cout << "Вещественных корней нет :(";
-        return;
-    }
-    std::cout << root1 << " " << root2 << std::endl;
+    if (a!=0){
+        double D = b*b - 4*a*c;
+        double root1 = ((b*-1)+sqrt(D))/(2*a);
+        double root2 = ((b*-1)-sqrt(D))/(2*a);
+        if (isnan(root1)||isnan(root2)) {
+            std::cout << "Вещественных корней нет :(";
+            return;
+        }
+        std::cout << root1 << " " << root2 << std::endl;
+    }else{
+        std::cout << -1*c/b << std::endl;
 }
 
 
